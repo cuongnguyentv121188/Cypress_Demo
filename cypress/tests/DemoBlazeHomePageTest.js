ec7098232b3c6c89a9afb2252e0ebdbc39ff9221
Steve Nguyen <minhcuongtv121188@gmail.com>
2026-03-28 18:13:20 -0600

    add new it(test case) for API testing (GET request)

--- cypress/tests/DemoBlazeHomePageTest.js
+++ cypress/tests/DemoBlazeHomePageTest.js
@@ -3,7 +3,7 @@ import products from "./DemoBlazeHomePage_products.json"
 
 describe ('DemoBlaze Home Page ' , () => { //mocha framework,
 
-    it('should be able to get all card data', () => {
+    xit('should be able to get all card data', () => {
         
         cy.visit("https://www.demoblaze.com/")
         new DemoBlazePage().getAllCardData().then(allCardData => {
@@ -12,6 +12,35 @@ describe ('DemoBlaze Home Page ' , () => { //mocha framework,
                 expect(allCardData).to.be.deep.eq(products);
             })
         })
+
+    });
+
+    it('should be able to get all card data', () => {
+        
+        cy.visit("https://www.demoblaze.com/");
+
+        // intercept default HomePage product
+        cy.intercept('/entries').as('entries') //intercept và đặt alias (tên gợi nhớ) bằng từ khóa AS
+        cy.wait('@entries') //chờ alias có KẾT QUẢ hiển thị
+        cy.get('@entries').then(entries => {
+            let aprProductData = entries.response.body.Items
+            aprProductData = aprProductData.map(item => {
+                return {
+                    itemName: item.title.replace('\n', ''),
+                    itemPrice: `$${item.price}`  // $ cho giống với format, $ thứ 2 là StringLiteral ${variable}
+                }
+            })
+
+            //cy.log(JSON.stringify(aprProductData)); 
+            // verify
+            new DemoBlazePage().getAllCardData().then(allCardData => {
+            cy.wrap('').then(() => {
+                //cy.log(JSON.stringify(allCardData))
+                expect(allCardData).to.be.deep.eq(aprProductData);
+            })
+        })
+        })
+
 
     });
 });
